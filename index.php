92b9113ce81352989869a89ce89bb6cd5a440203
Samar <osmansamar367@gmail.com>
2025-12-11 12:31:51 +0100

    Start to create

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- displays site properly based on user's device -->
+
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link rel="stylesheet" href="/schoenen/styles/style.css" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+  <link
+    href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght,YOPQ@100..900,300&family=Rasa:ital,wght@0,300..700;1,300..700&display=swap"
+    rel="stylesheet" />
+
+  <title>Frontend Mentor | E-commerce product page</title>
+
+  <!-- Feel free to remove these styles or customise in your own stylesheet 👍 -->
+  <!-- <style>
+      .attribution {
+        font-size: 11px;
+        text-align: center;
+      }
+      .attribution a {
+        color: hsl(228, 45%, 44%);
+      }
+    </style> -->
+</head>
+
+<body>
+  <!-- 
+  
+  These low-profile sneakers are your perfect casual wear companion. Featuring a 
+  durable rubber outer sole, they’ll withstand everything the weather can offer.
+  $125.00
+  50%
+  $250.00
+  0
+  Add to cart -->
+  <?php include 'header.php'; ?>
+  <?php include 'product.php'; ?>
+
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>-->
+
+
+  <script>
+    const minusBtn = document.querySelector(".minus");
+    const plusBtn = document.querySelector(".plus");
+    const qtyInput = document.getElementById("qtyInput");
+
+    plusBtn.addEventListener("click", () => {
+      qtyInput.value = parseInt(qtyInput.value) + 1;
+    });
+
+    minusBtn.addEventListener("click", () => {
+      const current = parseInt(qtyInput.value);
+      if (current > 0) {
+        qtyInput.value = current - 1;
+      }
+    });
+  </script>
+</body>
+
+</html>
---
+++ product.php
@@ -0,0 +1,43 @@
+<?php
+// Template Name: Product Page
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            hoi
+
+
+        </div>
+        <div class="col-12 col-lg-5">
+            <span class="sub-title">Sneaker Company</span>
+            <h1>Fall Limited Edition Sneakers</h1>
+            <p>
+                These low-profile sneakers are your perfect casual wear companion. Featuring a
+                durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+            <div class="price-section">
+                <span class="d-flex align-items-center gap-3 mb-1">
+                    <span class="current-price">$125.00</span>
+                    <span class="discount">50%</span>
+                </span>
+                <span class="original-price">$250.00</span>
+            </div>
+
+            <div class="quantity-addcart d-flex flex-column flex-md-row align-items-center gap-3 mt-4">
+                <!-- <input type="number" value="0" min="0" /> -->
+                <span class="qty-box">
+                    <img src="images/icon-minus.svg" class="qty-btn minus" />
+
+                    <input type="number" id="qtyInput" value="0" min="0" />
+
+                    <img src="images/icon-plus.svg" class="qty-btn plus" />
+                </span>
+                <button class="add-to-cart-btn">
+                    <span class="d-flex align-items-center gap-3">
+                        <img src="images/icon-cart.svg" alt="Cart Icon" class="cart-icon">
+                        Add to cart
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
